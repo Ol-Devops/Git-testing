13111d1f4013b44f819387701fb4ac800734170b
OL-Devops <oldevops27@gmail.com>
2024-10-14 21:24:24 +1000

    Added new form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   return (
     <>
       <h1>Git Testing</h1>
+
+      <form action="">
+        <div>
+          <p>Username</p>
+          <input type="text" />
+
+          <p>Password</p>
+          <input type="text" />
+        </div>
+      </form>
     </>
   );
 }
